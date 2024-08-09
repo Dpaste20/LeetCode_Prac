fb28c9f3b70de9d6be83749b88f1d115cf6427b1
Dhurv Paste <96135484+Dpaste20@users.noreply.github.com>
2024-08-09 20:53:27 +0530

    Create Best_time_buy_stock I

---
+++ Best_time_buy_stock.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+       # Time Complexity: O(n)
+       # Space Complexity : O(1)
+
+        min_price = float('inf')
+        max_profit = 0 
+
+        for price in prices:
+            if price < min_price:
+                min_price = price 
+            
+            profit = price - min_price
+
+            if profit > max_profit:
+                max_profit =  profit  
+            
+        
+        return max_profit
+        
